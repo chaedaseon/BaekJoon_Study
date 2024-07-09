03e653f77d8ddd99359ffc33cb36f96e0fd6c34b
ssony <156177607+chaedaseon@users.noreply.github.com>
2024-07-09 18:43:04 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 324 ms, Memory: 22572 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int i, j, k = 1;
+		
+		for (i=1; i<=num; i++)
+		{
+			for (j=1; j<=k; j++)
+			{
+				System.out.print("*");
+			}
+			
+			for (j=num; j>k; j--)
+			{
+				System.out.print("");
+			}
+			
+			k++;
+			System.out.println();
+		}
+	}
+}
